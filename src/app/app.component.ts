e4d4f2773b9bab7c56a8ab0d34815243427e58ce
Nnamdi <nnwabuokei@paga.com>
2020-10-08 10:43:32 +0100

    Added a title in app component, specifying its typings, created constructor that changes the intial title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  title:string = 'Angular work says hello-world';
+
+  constructor(){
+    this.title ="Welcome to the new normal";
+  }
 }
