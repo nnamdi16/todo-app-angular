c73f95b89996c9baa20fd339cfa85564cdb1a281
Nnamdi <nnwabuokei@paga.com>
2020-10-08 10:44:37 +0100

    Added all the components to app.module.ts and also added the CourseService to the provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { CourseComponent } from './components/course/course.component';
+import { CoursesService } from './components/course/courses.service';
+import { HomeComponent } from './components/home/home.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemsComponent } from './components/todo-items/todo-items.component';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CourseComponent, HomeComponent, AboutComponent, TodosComponent, TodoItemsComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [CoursesService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
