444d07f2967d435b349b8d5378154b6090340a4f
Nnamdi <nnwabuokei@paga.com>
2020-10-10 13:13:55 +0100

    refactor the component's hero property to be of type Hero, initialize it with an id of 1 and the name Windstorm

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from 'src/app/models/Hero';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
   constructor() { }
 
   ngOnInit(): void {
