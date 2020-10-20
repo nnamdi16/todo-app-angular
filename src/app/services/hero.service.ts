39495dcb8cdf765e8580a8634e5e17bf293e960b
Nnamdi <nnwabuokei@paga.com>
2020-10-20 22:11:28 +0100

    injected messageService into HeroService

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,3 +1,4 @@
+import { MessageService } from './message.service';
 import { Observable, of } from 'rxjs';
 import { HEROES } from './../data/mock-heroes';
 import { Hero } from './../models/Hero';
@@ -8,9 +9,10 @@ import { Injectable } from '@angular/core';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes')
     return of(HEROES);
   }
 }
