cb1ccbb70987fdf32d401233e4b58473e46ac7a3
Nnamdi <nnwabuokei@paga.com>
2020-10-20 21:33:55 +0100

    import Observables and of for making asynchronous calls

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,3 +1,4 @@
+import { Observable, of } from 'rxjs';
 import { HEROES } from './../data/mock-heroes';
 import { Hero } from './../models/Hero';
 import { Injectable } from '@angular/core';
@@ -9,7 +10,7 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes():Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
