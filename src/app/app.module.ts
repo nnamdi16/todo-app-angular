328339c133fbfbfadc1086e63b42aaf2bf039908
Nnamdi <nnwabuokei@paga.com>
2020-10-22 22:58:40 +0100

    created a dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,13 @@ import { HeroComponent } from './components/hero/hero.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { MessageComponent } from './components/message/message.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 
 
 @NgModule({
-  declarations: [AppComponent, CourseComponent, HomeComponent, AboutComponent, TodosComponent, TodoItemsComponent, HeroComponent, HeroDetailComponent, MessageComponent],
+  declarations: [AppComponent, CourseComponent, HomeComponent, AboutComponent, TodosComponent, TodoItemsComponent, HeroComponent, HeroDetailComponent, MessageComponent, DashboardComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
