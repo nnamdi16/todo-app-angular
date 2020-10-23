d763a31a3915a1f8d3a9ada6d7bf1ded3a247d6b
Nnamdi <nnwabuokei@paga.com>
2020-10-23 16:10:20 +0100

    injected ActivatedRoute, HeroServicenad Location services into the constructor

--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
+import { HeroService } from './../../services/hero.service';
 import { Hero } from './../../models/Hero';
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,7 +11,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class HeroDetailComponent implements OnInit {
   
-  @Input() hero:Hero;
+  // @Input() hero:Hero;
+  hero: Hero;
   
   constructor(
     private route: ActivatedRoute,
@@ -22,6 +26,9 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero():void {
+    // route.snapshpt is a static image of the route information shortly after the component was created 
+    // the parmaMap is a dictionary of route parameter values extracted from the URL. The "id" key returns the id of the hero to fetch
+    //The + sign converts the string to a number, which is what hero id should since route parmaeters are always string
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
